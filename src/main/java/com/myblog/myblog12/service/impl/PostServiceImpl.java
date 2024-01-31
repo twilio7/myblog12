c66f5e8f1727a201f5e02c19eb830a2c0517e974
twilio5 <twilio.sms01@gmail.com>
2024-01-31 20:31:50 +0530

    128:Create save Post Feature entity class 31 jan 2024

---
+++ src/main/java/com/myblog/myblog12/service/impl/PostServiceImpl.java
@@ -0,0 +1,38 @@
+package com.myblog.myblog12.service.impl;
+
+import com.myblog.myblog12.entity.Post;
+import com.myblog.myblog12.payload.PostDto;
+import com.myblog.myblog12.repository.PostRepository;
+import com.myblog.myblog12.service.PostService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PostServiceImpl implements PostService {
+
+private PostRepository postRepository;
+
+    public PostServiceImpl(PostRepository postRepository) {
+
+        this.postRepository = postRepository;
+    }
+
+    @Override
+    public PostDto createPost(PostDto postDto) {
+        Post post = new Post();
+        post.setTitle(postDto.getTitle());
+        post.setDescription(postDto.getDescription());
+        post.setContent(postDto.getContent());
+        Post savedPost = postRepository.save(post);
+
+        PostDto dto = new PostDto();
+
+        dto.setTitle(savedPost.getTitle());
+        dto.setDescription(savedPost.getDescription());
+        dto.setContent(savedPost.getContent());
+
+
+
+
+        return dto;
+    }
+}
