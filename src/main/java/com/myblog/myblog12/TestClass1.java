81e2187f993b8de12eab2aebbbaaede6fc739fb6
twilio5 <twilio.sms01@gmail.com>
2024-02-01 17:01:38 +0530

    128:Create save Post Feature entity class 1 feb filter realted program

--- src/main/java/com/myblog/myblog12/TestClass1.java
+++ src/main/java/com/myblog/myblog12/TestClass1.java
@@ -2,18 +2,17 @@ package com.myblog.myblog12;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 
 public class TestClass1 {
-    public static void main(String[] args) {
-        List<String> numbers  = Arrays.asList("deepak","virendra","deepak","sonu");
-        List<String>  data1 = numbers.stream().filter(s-> s.startsWith("v")).collect(Collectors.toList());
-        List<String>  data2 = numbers.stream().filter(s-> s.equals("deepak")).collect(Collectors.toList());
-        List<String>  data3 = numbers.stream().filter(s-> s.endsWith("u")).collect(Collectors.toList());
-        System.out.println(data1);
-        System.out.println(data2);
-        System.out.println(data3);
-    }
+     public static void main(String[] args) {
+     List<Integer> data  = Arrays.asList(15,20,80,1,15,20,80);
+         List<Integer> newData = data.stream().distinct().sorted().collect(Collectors.toList());
+         System.out.println(newData);
+
+
+     }
 }
